fd46dbbe0a5f9cd4ccc812036825a22f00441e7e
Matei Radu <mateisr@proton.me>
2025-10-27 18:03:37 +0200

    Switch to "Murder Custom" gamemode
    change file tree and content to show "Murder Custom" in the gamemode selection menu

--- gamemodes/murder_custom/gamemode/shared.lua
+++ gamemodes/murder_custom/gamemode/shared.lua
@@ -1,10 +1,10 @@
-GM.Name 	= "Murder"
+GM.Name 	= "MurderCustom"
 GM.Author 	= "MechanicalMind"
 -- credits to Minty Fresh for some styling on the scoreboard
 -- credits to Waddlesworth for the logo and menu icon
 GM.Email 	= ""
 GM.Website 	= "www.codingconcoctions.com/murder/"
-GM.Version = "29"
+GM.Version = "30"
 
 function GM:SetupTeams()
 	team.SetUp(1, translate.teamSpectators, Color(150, 150, 150))
